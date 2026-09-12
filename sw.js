5c9a244077de8559708e0a3852641341b43eda87
Lau <l.somacarrera@gmail.com>
2026-09-12 14:32:23 +0200

    Add files via upload
    ZeroStressHome_v10_unirse_codigo_y_cache

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
-const CACHE="zerostresshome-v1";
+const CACHE="zerostresshome-v2";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./firebase-config.js","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+
+self.addEventListener("install",e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+));
+
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k.startsWith("zerostresshome-") && k!==CACHE).map(k=>caches.delete(k))
+  )).then(()=>self.clients.claim())
+));
+
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
   const url=new URL(e.request.url);
   if(url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
-      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
-    })));
+    e.respondWith(
+      fetch(e.request).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return res;
+      }).catch(()=>caches.match(e.request))
+    );
   }
 });
